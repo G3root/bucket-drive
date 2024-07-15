e2763524bcc37052ce437cce0ef34ac956fe2d43
nafees nazik <nafeesnazik21@gmail.com>
2024-07-15 18:25:58 +0530

    feat: fix layout

---
+++ app/routes/_dashboard+/__components/header.tsx
@@ -0,0 +1,41 @@
+import { NavLink } from "@remix-run/react";
+import clsx from "clsx";
+
+type NavLinks = {
+  label: string;
+  to: string;
+};
+
+const NavLinks: NavLinks[] = [
+  {
+    label: "Folders",
+    to: "/",
+  },
+  {
+    label: "Settings",
+    to: "/settings",
+  },
+];
+
+export function Header() {
+  return (
+    <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+      <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+        {NavLinks.map((item) => (
+          <NavLink
+            key={item.to}
+            to={item.to}
+            className={({ isActive }) =>
+              clsx(
+                "transition-colors hover:text-foreground",
+                isActive ? "text-foreground" : "text-muted-foreground"
+              )
+            }
+          >
+            {item.label}
+          </NavLink>
+        ))}
+      </nav>
+    </header>
+  );
+}
